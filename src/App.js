8f438593be3b74ca9d035f4c17b5058210ed59ba
Beata Kosmider <kosmibe@gmail.com>
2023-04-11 09:46:10 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   constructor(props) {
     super();
 
     this.state = {
-      isLoading: true
-    }
+      isLoading: true,
+    };
 
     setTimeout(() => {
-      this.setState({isLoading: false})
-    }, 2000)
+      this.setState({ isLoading: false });
+    }, 2 * 1000);
   }
   render() {
-    if (this.state.isLoading) return <h1>Loading</h1>
-    else return <h1>Witaj</h1>
+    if (this.state.isLoading) return <h1>Loading</h1>;
+    else return <h1>Witaj</h1>;
   }
 }
 
